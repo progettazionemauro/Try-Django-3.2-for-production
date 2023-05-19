0e94efefb2a11cdd4dd7c8c70d773c79940aa4a5
progettazionemauro <progettazione@studiogb.org>
2023-05-19 08:09:21 +0200

    inserito allowed host

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-0+dmu6*lky0l74
 DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
 
 ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sea-turtle-app-zn46g.ondigitalocean.app']
 if not DEBUG:
     ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
 
